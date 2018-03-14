3faebd518865e9225f68113659d01c9e16634cc6
gza21 <gza21@asb9838nu-d09.csil.sfu.ca>
2018-03-14 15:48:38 -0700

    lab2

--- lab2/subprog.c
+++ lab2/subprog.c
@@ -2,8 +2,15 @@ typedef unsigned short bit16;
 char *cvt2bitstr(bit16 x, char* str) {
     int i;
     for (i = 0; i < 16; i++) {
-        str[i] = (char) ((x & 0x8000) >> 15) | 0x30;
-        x = (x << 1) & 0xffff;
+        if(x!=0x0){
+            str[i] = (char) ((x & 0x8000) >> 15) | 0x30;
+            x = (x << 1) & 0xffff;
+        }
+        else{
+            str[i]='0';
+        }
+        
+        
     }
     str[16] = '\0';
     return str;
